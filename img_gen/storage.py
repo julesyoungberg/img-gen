bb4c07dd7d27aced066dc1fafd7037006536142a
Jules Youngberg <julesdayoungberg@gmail.com>
2021-07-26 13:51:40 -0700

    try to fix uploading for colab

--- img_gen/storage.py
+++ img_gen/storage.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def save_figure(fig, key):
     print("saving figure")
     plt.savefig("temp.png", dpi=300, bbox_inches="tight")
-    client = storage.Client.from_service_account_json("key.json")
+    client = storage.Client()
     bucket = client.get_bucket("img-gen-training")
     blob = bucket.blob(key)
     blob.upload_from_filename(filename="temp.png")
