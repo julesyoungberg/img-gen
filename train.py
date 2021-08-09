901af872e03d49a0ddf5a81a9fcc5092a38b9af8
Jules Youngberg <julesdayoungberg@gmail.com>
2021-08-09 14:55:18 -0700

    update training scripts'

--- train.py
+++ train.py
@@ -1,38 +1,13 @@
-# import tensorflow_cloud as tfc
-
 GCP_PROJECT = "img-gen-319216"
 GCP_BUCKET = "img-gen-training"
-
-# tfc.run(
-#     requirements_txt="requirements.txt",
-#     stream_logs=True,
-#     worker_count=0,
-#     # https://github.com/tensorflow/cloud/blob/d509c231d6b2efec34a0af5da0ee02535a1f746d/src/python/tensorflow_cloud/core/machine_config.py#L116
-#     chief_config=tfc.COMMON_MACHINE_CONFIGS["K80_8X"],
-# )
 
 import tensorflow_datasets as tfds
 
 from img_gen.cycle_gan import CycleGAN, find_optimal_cycle_gan
 from img_gen.img import preprocess_images
+from img_gen.data import load_cycle_gan_dataset
 
-# load the data set
-print("loading data")
-data, metadata = tfds.load(
-    "cycle_gan/vangogh2photo",
-    with_info=True,
-    as_supervised=True,
-)
-
-train_x, train_y = data["trainA"], data["trainB"]
-test_x, test_y = data["testA"], data["testB"]
-
-# preprocess data
-print("preprocessing data")
-train_x = preprocess_images(train_x, jitter=True)
-train_y = preprocess_images(train_y, jitter=True)
-test_x = preprocess_images(test_x)
-test_y = preprocess_images(test_y)
+train_x, train_y, test_x, test_y = load_cycle_gan_dataset("vangogh2photo")
 
 # build & train the model
 print("finding optimal cycle gan")
@@ -46,19 +21,16 @@ cyc_gan = find_optimal_cycle_gan(
     cloud_project=GCP_PROJECT,
     cloud_bucket=GCP_BUCKET,
 )
-# cyc_gan = CycleGAN(gen_conv_size=3)
-
-# cyc_gan.train(train_x, train_y, test_x, test_y, epochs=1)
 
 # print results
-# cyc_gan.print_losses()
-# cyc_gan.plot_losses()
+cyc_gan.print_losses()
+cyc_gan.plot_losses()
 
-# print("Test Results")
-# cyc_gan.generate_images(test_x, test_y)
+print("Test Results")
+cyc_gan.generate_images(test_x, test_y)
 
-# gen_g_loss, gen_f_loss, dis_x_loss, dis_y_loss = cyc_gan.scores(test_x, test_y)
-# print("Test Losses")
-# print(
-#     f"gen_g: {gen_g_loss}, gen_f: {gen_f_loss}, dis_x: {dis_x_loss}, dis_y: {dis_y_loss}"
-# )
+gen_g_loss, gen_f_loss, dis_x_loss, dis_y_loss = cyc_gan.scores(test_x, test_y)
+print("Test Losses")
+print(
+    f"gen_g: {gen_g_loss}, gen_f: {gen_f_loss}, dis_x: {dis_x_loss}, dis_y: {dis_y_loss}"
+)
